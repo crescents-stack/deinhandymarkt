09eb9ac86c83c9a4dae6c612f13094cf284294e8
MusiurAlamOpu <musiur.opu@gmail.com>
2024-02-07 17:49:14 +0600

    image type changed while uploding

--- src/app/dashboard/categories/add/page.tsx
+++ src/app/dashboard/categories/add/page.tsx
@@ -106,8 +106,8 @@ const Page = () => {
                         form.setValue("icon", e.target.value);
                       }}
                       name="icon"
-                      accept=".svg"
-                      sizeLimit={100}
+                      accept=".svg, .png, .jpg, .jpeg, .avif, .webp"
+                      sizeLimit={500}
                       defaultValue={form.getValues("icon")}
                     />
                   </FormControl>
