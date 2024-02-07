b9a23dbc0e81fb157fa3f2c5a6c48c0c956a879a
MusiurAlamOpu <musiur.opu@gmail.com>
2024-02-07 17:41:28 +0600

    click event added to products

--- src/components/molecules/products-card.tsx
+++ src/components/molecules/products-card.tsx
@@ -2,34 +2,31 @@ import { TProductSchema } from "@/app/dashboard/products/_utils/types/types";
 import Image from "next/image";
 import Link from "next/link";
 
-const ProductsCard = ({
-  details,
-}: {
-  details: TProductSchema;
-}) => {
+const ProductsCard = ({ details }: { details: TProductSchema }) => {
   const { name, thumbnail, price, slug } = details;
   // PRINT(details);
   return (
-    <div className="flex flex-col items-center justify-center gap-[10px] sm:gap-[20px] px-[10px] md:px-[40px] py-[40px]">
-      <Image
-        unoptimized
-        src={thumbnail}
-        alt="product-thumbnail"
-        width={1000}
-        height={1000}
-        className="max-w-[100px] sm:max-w-[222px] mx-auto"
-      />
-      <div className="flex flex-col items-center justify-center gap-[16px]">
-        {/* <p className="text-secondary text-[12px] md:text-[14px]">{arrival}</p> */}
-        <Link href={`/products/${slug.replaceAll(" ", "").replaceAll("%","")}`}>
+    <Link href={`/products/${slug.replaceAll(" ", "").replaceAll("%", "")}`}>
+      <div className="flex flex-col items-center justify-center gap-[10px] sm:gap-[20px] px-[10px] md:px-[40px] py-[40px]">
+        <Image
+          unoptimized
+          src={thumbnail}
+          alt="product-thumbnail"
+          width={1000}
+          height={1000}
+          className="max-w-[100px] sm:max-w-[222px] mx-auto"
+        />
+        <div className="flex flex-col items-center justify-center gap-[16px]">
+          {/* <p className="text-secondary text-[12px] md:text-[14px]">{arrival}</p> */}
+
           <h4 className="text-center max-w-[300px] mx-auto text-[12px] md:text-[14px] font-bold group-hover:text-secondary group-hover:underline">
             {name}
           </h4>
-        </Link>
-        <p>${price}.00</p>
-      </div>
 
-      {/* {colors ? (
+          <p>${price}.00</p>
+        </div>
+
+        {/* {colors ? (
         <div className="flex flex-wrap items-center justify-center gap-[2px] sm:gap-[4px] md:gap-[8px]">
           {colors.map((item: any) => {
             return (
@@ -49,7 +46,8 @@ const ProductsCard = ({
           })}
         </div>
       ) : null} */}
-    </div>
+      </div>
+    </Link>
   );
 };
 
