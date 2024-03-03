6a1f842afc91d3a4de6b7fb3e8cc4e02e6fe4ccb
MusiurAlamOpu <musiur.opu@gmail.com>
2024-03-03 17:57:22 +0600

    Page view updated

--- src/lib/gtag-helper.ts
+++ src/lib/gtag-helper.ts
@@ -1,5 +1,6 @@
 export const pageview = (GA_MEASUREMENT_ID : string, url : string) => {
     window.gtag("config", GA_MEASUREMENT_ID, {
+        event: "cookie_consent_update",
         page_path: url,
     });
 };
